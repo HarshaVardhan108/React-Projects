82c2c360fbc5aae1cdf3db34dbb4bc2c5e2224e3
HarshaVardhan <umapathi2harsha@gmail.com>
2025-07-23 18:03:22 +0530

    React-Projects

---
+++ BG-ColorApp/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useCallback, useState } from "react";
+
+function App() {
+  // const handleClick = (e) => {
+  //   const body = document.querySelector('body');
+  //   body.style.background = getRandomColor();
+  // }
+
+  // function getRandomColor() {
+  //   let letters = '0123456789ABCDEF'
+  //   let colors = '#'
+  //   for (let i = 0; i < 6; i++) {
+  //     colors += letters[Math.floor(Math.random()*16)]
+  //   }
+  //   return colors;
+  // }
+  // OPTIMIZED CODE FOR REACT
+
+  const [bgColor, setBgColor] = useState("#FFFFFF");
+
+  const getRandomColor = useCallback(() => {
+    const letters = "0123456789ABCDEF";
+    let colors = "#";
+    for (let i = 0; i < 6; i++) {
+      colors += letters[Math.floor(Math.random() * 16)];
+    }
+    return colors;
+  }, []);
+
+  const handleClick = useCallback(() => {
+    setBgColor(getRandomColor());
+  }, [getRandomColor]);
+
+  return (
+    <>
+      <div style={{backgroundColor:bgColor}}>
+        <div className="container">
+          <h1>Random Background Color Changer</h1>
+          <button className="btn" onClick={handleClick}>
+            Cilck me
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Esign App/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+
+  return (
+    <>
+      <div>
+        <h1>Signature app</h1>
+        <h3>{name}</h3>
+        <h3>{date}</h3>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam
+          tenetur inventore iure sequi molestiae repellat, esse sint expedita
+          recusandae porro?
+        </p>
+      </div>
+
+      <div>
+        <input type="date" name="" id="" onChange={(e) => setDate(e.target.value)} />
+        &nbsp;
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+      </div>
+
+    </>
+  );
+}
+
+export default App;
